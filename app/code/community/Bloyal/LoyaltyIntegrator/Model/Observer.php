0ec36e6a6661d16f8a643a9692b14cfee43883bf
root <root@user-549>
2014-03-05 10:51:46 +0530

    giftcart functionality added

---
+++ app/code/community/Bloyal/LoyaltyIntegrator/Model/Observer.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * bLoyal
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade bLoyal to newer
+ * versions in the future. If you wish to customize bLoyal for your
+ * needs please refer to http://www.bloyal.com for more information.
+ *
+ * @category    Model
+ * @package     Model_Observer
+ * @copyright   Copyright (c) 2012 bLoyal Inc. (http://www.bloyal.com)
+ * @license     http://www.bloyal.com
+ */
+
+/**
+ * Media library Image model
+ *
+ * @category   Bloyal
+ * @package    Bloyal_LoyaltyIntegrator
+ */
+class Bloyal_LoyaltyIntegrator_Model_Observer {
+
+    /**
+     * Function to unset bloayl message session
+     * @param Varien_Event_Observer $observer
+     */
+    public function unsetBloaylSession(Varien_Event_Observer $observer) {
+        $event = $observer->getEvent();
+        Mage::getSingleton('core/session')->setBloyalCouponCode('');
+        Mage::log($event->getName(), Zend_Log::DEBUG, 'loyalty.log');
+    }
+    
+    
+
+    /**
+     * Function to apply bLoyal Discount code.
+     * @param Varien_Event_Observer $observer
+     */
+    public function applyCouponDiscount(Varien_Event_Observer $observer) {
+
+        $quote = $observer->getEvent()->getQuote();
+        $quoteid = $quote->getId();
+        $couponCode = $quote->getCouponCode();
+
+        $arrData = Mage::app()->getRequest()->getPost();
+
+        $updatedAt = date('U', strtotime($observer->getQuote()->getUpdatedAt()));
+        $now = time();
+        if (($updatedAt + 3) > $now) {
+
+            if (Mage::getSingleton('core/session')->getBloyalCouponCode() && !$couponCode) {
+                Mage::getSingleton('checkout/session')->getMessages(true);
+                if ($strMessage = Mage::getSingleton('checkout/session')->getBloyalMsg()) {
+                    Mage::getSingleton('checkout/session')->getMessages(true);
+                    if ($strMessage === 'Success')
+                        Mage::getSingleton('checkout/session')->addSuccess('Coupon code "BL20" was applied.');
+                    elseif ($strMessage === 'Fail') {
+                        Mage::getSingleton('checkout/session')->addError('Coupon code "BL20" is Invalid.');
+                    }
+                }
+            }
+
+            Mage::log('Success 2', Zend_Log::DEBUG, 'loyalty.log');
+        }
+
+        if (!$couponCode) {
+            if ($arrData['coupon_code'] && !$arrData['remove']) {
+                // Success
+                Mage::getSingleton('core/session')->setBloyalCouponCode($arrData['coupon_code']);
+            }
+
+            if ($arrData['remove']) {
+                //Error 
+                Mage::getSingleton('core/session')->setBloyalCouponCode('');
+            }
+        } else {
+            Mage::getSingleton('core/session')->setBloyalCouponCode('');
+        }
+
+        if (!$couponCode && Mage::getSingleton('core/session')->getBloyalCouponCode()) {
+            if (Mage::getSingleton('core/session')->getBloyalCouponCode() === 'SALE') {
+                $discountAmount = 20;
+                Mage::getSingleton('checkout/session')->setBloyalMsg('Success');
+            } else {
+                $discountAmount = 0;
+                Mage::getSingleton('checkout/session')->setBloyalMsg('Fail');
+            }
+
+            if ($quoteid) {
+                if ($discountAmount > 0) {
+
+                    $total = $quote->getBaseSubtotal();
+                    $quote->setSubtotal(0);
+                    $quote->setBaseSubtotal(0);
+
+                    $quote->setSubtotalWithDiscount(0);
+                    $quote->setBaseSubtotalWithDiscount(0);
+
+                    $quote->setGrandTotal(0);
+                    $quote->setBaseGrandTotal(0);
+
+
+                    $canAddItems = $quote->isVirtual() ? ('billing') : ('shipping');
+                    foreach ($quote->getAllAddresses() as $address) {
+
+                        $address->setSubtotal(0);
+                        $address->setBaseSubtotal(0);
+
+                        $address->setGrandTotal(0);
+                        $address->setBaseGrandTotal(0);
+
+                        $address->collectTotals();
+
+                        $quote->setSubtotal((float) $quote->getSubtotal() + $address->getSubtotal());
+                        $quote->setBaseSubtotal((float) $quote->getBaseSubtotal() + $address->getBaseSubtotal());
+
+                        $quote->setSubtotalWithDiscount(
+                                (float) $quote->getSubtotalWithDiscount() + $address->getSubtotalWithDiscount()
+                        );
+                        $quote->setBaseSubtotalWithDiscount(
+                                (float) $quote->getBaseSubtotalWithDiscount() + $address->getBaseSubtotalWithDiscount()
+                        );
+
+                        $quote->setGrandTotal((float) $quote->getGrandTotal() + $address->getGrandTotal());
+                        $quote->setBaseGrandTotal((float) $quote->getBaseGrandTotal() + $address->getBaseGrandTotal());
+
+                        $quote->save();
+
+                        $quote->setGrandTotal($quote->getBaseSubtotal() - $discountAmount)
+                                ->setBaseGrandTotal($quote->getBaseSubtotal() - $discountAmount)
+                                ->setSubtotalWithDiscount($quote->getBaseSubtotal() - $discountAmount)
+                                ->setBaseSubtotalWithDiscount($quote->getBaseSubtotal() - $discountAmount)
+                                ->save();
+
+
+
+                        if ($address->getAddressType() == $canAddItems) {
+                            //echo $address->setDiscountAmount; exit;
+                            $address->setSubtotalWithDiscount((float) $address->getSubtotalWithDiscount() - $discountAmount);
+                            $address->setGrandTotal((float) $address->getGrandTotal() - $discountAmount);
+                            $address->setBaseSubtotalWithDiscount((float) $address->getBaseSubtotalWithDiscount() - $discountAmount);
+                            $address->setBaseGrandTotal((float) $address->getBaseGrandTotal() - $discountAmount);
+                            if ($address->getDiscountDescription()) {
+                                $address->setDiscountAmount(-($address->getDiscountAmount() - $discountAmount));
+                                $address->setDiscountDescription($address->getDiscountDescription() . ', Custom Discount');
+                                $address->setBaseDiscountAmount(-($address->getBaseDiscountAmount() - $discountAmount));
+                            } else {
+                                $address->setDiscountAmount(-($discountAmount));
+                                $address->setDiscountDescription('Custom Discount');
+                                $address->setBaseDiscountAmount(-($discountAmount));
+                            }
+                            $address->save();
+                        }//end: if
+                    } //end: foreach
+                    //echo $quote->getGrandTotal();
+
+                    foreach ($quote->getAllItems() as $item) {
+                        //We apply discount amount based on the ratio between the GrandTotal and the RowTotal
+                        $rat = $item->getPriceInclTax() / $total;
+                        $ratdisc = $discountAmount * $rat;
+                        $item->setDiscountAmount(($item->getDiscountAmount() + $ratdisc) * $item->getQty());
+                        $item->setBaseDiscountAmount(($item->getBaseDiscountAmount() + $ratdisc) * $item->getQty())->save();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ app/code/community/Bloyal/Master/Model/Abstract.php
@@ -0,0 +1,334 @@
+<?php
+
+/**
+ * bLoyal
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade bLoyal to newer
+ * versions in the future. If you wish to customize bLoyal for your
+ * needs please refer to http://www.bloyal.com for more information.
+ *
+ * @category    Bloyal
+ * @package     Bloyal_Master
+ * @copyright   Copyright (c) 2014 bLoyal Inc. (http://www.bloyal.com)
+ * @license     http://www.bloyal.com
+ */
+
+/**
+ * Captcha block
+ *
+ * @category   Community
+ * @package    Bloyal_Master
+ * @author     Bloyal Team
+ */
+class Bloyal_Master_Model_Abstract extends Mage_Core_Model_Abstract {
+
+    /**
+     * Api resource
+     *
+     * @var String
+     */
+    protected $_api = '';
+
+    /**
+     * Mage Api resource
+     * 
+     * @var string
+     */
+    protected $_mageApi = '';
+
+    /**
+     * credentials
+     *
+     * @var string
+     */
+    protected $_credentials = '';
+
+    /**
+     * Session ID
+     *
+     * @var string
+     */
+    protected $_sessionId = '';
+
+    /**
+     * Default socket timeout
+     *
+     * @var string
+     */
+    protected $_defaultSocketTimeout = '';
+
+    /**
+     * Default mysql timeout
+     *
+     * @var string
+     */
+    protected $_defaultMysqlTimeout = '';
+
+    /**
+     * Product details data
+     *
+     * @var array
+     */
+    protected $_productData = '';
+
+    /**
+     * Function to unset Mage Api 
+     *
+     * @param none
+     * @return unknown
+     */
+    protected function unsetMageApi() {
+
+        if ($this->_defaultSocketTimeout)
+            ini_set('default_socket_timeout', $this->_defaultSocketTimeout);
+        if ($this->_defaultMysqlTimeout)
+            ini_set('mysql.connect_timeout', $this->_defaultMysqlTimeout);
+        if ($this->_mageApi)
+            unset($this->_mageApi, $this->_sessionId, $this->_defaultSocketTimeout, $this->_defaultMysqlTimeout);
+
+        return $this;
+    }
+
+    /**
+     * Function to get mage Client
+     *
+     * @param none
+     * @return unknown
+     */
+    public function getMageClient() {
+
+        if (empty($this->_sessionId)) {
+
+            // First unset mage pai
+            $this->unsetMageApi();
+
+            //Set default socket timeout
+            $this->_defaultSocketTimeout = ini_get('default_socket_timeout');
+            ini_set('default_socket_timeout', 300);
+
+            // Set default mysql timeout
+            $this->_defaultMysqlTimeout = ini_get('mysql.connect_timeout');
+            ini_set('mysql.connect_timeout', 300);
+
+            try {
+
+                $helper = Mage::helper('bloyalMaster');
+                $_helper = Mage::helper('bloyalCatalog');
+                $magentoUser = $helper->getBloyalConfig('general/magento_admin');
+                $magentoPass = $helper->getBloyalConfig('general/magento_pass');
+                if (trim($magentoUser) == '' || trim($magentoPass) == '') {
+                    // Send notification for No Store Found
+                    $_helper->sendNotification('Create magento user and password for SOAP Service as intruction give in installation guide and completed bloyal setting .', '');
+
+                    // Write into log file for that
+                    $_helper->log('Create magento user and password for SOAP Service as intruction give in installation guide and completed bloyal setting .', Bloyal_CatalogIntegrator_Model_Catalog::EXCEPTION_FILE);
+                    $_helper->log('Create magento user and password for SOAP Service as intruction give in installation guide and completed bloyal setting .', Bloyal_CatalogIntegrator_Model_Catalog::REGULAR_FILE);
+
+                    return false;
+                }
+
+                $this->_mageApi = new SoapClient(Mage::getUrl() . 'api/soap?wsdl');
+                $this->setSessionId($this->_mageApi->login($magentoUser, $magentoPass));
+            } catch (Exception $e) {
+
+                $this->unsetMageApi();
+                return false;
+            }
+        } else {
+            return $this->_mageApi;
+        }
+
+        return $this->_mageApi;
+    }
+
+    /**
+     * Get Session ID
+     *
+     * @param none
+     * @return string
+     */
+    public function getSessionId() {
+
+        return $this->_sessionId;
+    }
+
+    /**
+     * Set session ID
+     *
+     * @param String $id
+     * @return object
+     */
+    public function setSessionId($id) {
+
+        $this->_sessionId = $id;
+        return $this;
+    }
+
+    /**
+     * Send curl request
+     *
+     * @param string $action
+     * @return String
+     */
+    protected function sendCurlRequest($action = '') {
+        $helper = Mage::helper('bloyalMaster');
+        try {
+
+
+
+            $strBaseApiUrl = $helper->apiUrl;
+            $strApiDemo = $helper->getBloyalConfig('general/domain');
+            $strUserName = $helper->getBloyalConfig('general/username');
+            $strPassword = $helper->getBloyalConfig('general/password');
+
+            $strContent = '';
+
+            if ($action !== '' && $action != null) {
+                $posturl = $strBaseApiUrl . '/' . $strApiDemo . '/' . $action;
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $posturl);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_USERPWD, "$strUserName:$strPassword");
+                curl_setopt($ch, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml"));
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $strContent);
+                curl_setopt($ch, CURLOPT_POST, 0);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+                $response = curl_exec($ch);
+                return $response;
+            }
+        } catch (Exception $e) {
+            $helper->logException($e, Bloyal_CatalogIntegrator_Model_Catalog::EXCEPTION_FILE);
+        }
+        return false;
+    }
+
+    /**
+     * Parse Curl response
+     *
+     * @param string $strAction 
+     * @return Object
+     */
+    public function getCurlResponse($strAction) {
+        $helper = Mage::helper('bloyalMaster');
+
+        if ($response = $this->sendCurlRequest($strAction)) {
+            return $helper->parseXmlToArray($response);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Create Soap products into magento
+     *
+     * @param $products array()
+     * @return Integrer $productId
+     */
+    protected function createSoapProducts($products) {
+        $_helper = Mage::helper('bloyalCatalog');
+        $attributeSet = $_helper->getGeneralConfig('general/product_attributeset');
+
+        //Create products into magento from bloyal
+        $productId = $this->getMageClient()->call($this->getSessionId(), 'catalog_product.create', array('simple', $attributeSet, 'product_sku', $products));
+
+        $this->updateSoapInventory($productId, $products);
+
+        return $productId;
+    }
+
+    /**
+     * Update Soap products into magento
+     *
+     * @param $productId Integer
+     * @param $products array()
+     * @return Boolean
+     */
+    protected function updateSoapProducts($productId, $products) {
+        // Set arguments
+        $arguments = array($productId, $products);
+
+        // Update products into magento from bloyal
+        $boolean = $this->getMageClient()->call($this->getSessionId(), 'catalog_product.update', $arguments);
+
+        //Update stock inventory
+        $this->updateSoapInventory($productId, $products);
+
+        // return boolean for update
+        return $boolean;
+    }
+
+    /**
+     * Get product info from magento
+     *
+     * @param $strSku String
+     * @return Integer 
+     */
+    protected function getMagentoProductsInfo($strSku) {
+        // get Product info from magento 
+        $result = $this->getMageClient()->call($this->getSessionId(), 'catalog_product.info', $strSku);
+
+        if (isset($result['product_id']) && $result['product_id']) {
+            return $result['product_id'];
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Update Soap products stock into magento
+     *
+     * @param $productId Integer
+     * @param $products array()
+     * @return 
+     */
+    private function updateSoapInventory($productId, $arrInventoryData) {
+
+        $productsInventory = array(
+            'qty' => $arrInventoryData['qty'],
+            'is_in_stock' => ($arrInventoryData['qty'] ? 1 : 0),
+            'manage_stock' => 1,
+        );
+
+        // Set arguments
+        $arguments = array($productId, $productsInventory);
+
+        // Update products stock into magento from bloyal
+        $result = $this->getMageClient()->call($this->getSessionId(), 'product_stock.update', $arguments);
+    }
+
+    public function getApi() {
+
+        if (!$this->_api) {
+            $helper = Mage::helper('bloyalMaster');
+            try {
+                $this->_api = @new SoapClient($helper->getBloyalConfig('general/bloyal_api_url'));
+            } catch (Exception $e) {
+                $helper->sendNotification('Connectivity problem', '');
+                return false;
+            }
+        }
+        return $this->_api;
+    }
+
+    public function getCredentials() {
+
+        if (!$this->_credentials) {
+
+            $helper = Mage::helper('bloyalMaster');
+            $this->_credentials = array('Domain' => $helper->getBloyalConfig('general/domain'),
+                'UserName' => $helper->getBloyalConfig('general/username'),
+                'Password' => $helper->getBloyalConfig('general/password'),
+                'ApplicationId' => $helper->getBloyalConfig('general/app_id'),
+                'DeviceKey' => $helper->getBloyalConfig('general/device_key'));
+        }
+
+        return $this->_credentials;
+    }
+
+}
